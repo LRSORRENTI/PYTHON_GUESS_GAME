5ea6a7b10fd75e0c841ad7c07d45eab5631775f5
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-11 19:00:36 -0500

    added notes on the random module, also initialized constant variables to house the code that will be guessed

---
+++ codeGuessingGame.py
@@ -0,0 +1,56 @@
+# first import the random module 
+
+# The random module in Python is a 
+# built-in module that provides a 
+# random number generator. It can be 
+# used to generate random numbers, shuffle lists,
+# and perform other random operations. The
+# random module is a powerful tool that can 
+# be used for a variety of tasks, such as:
+
+# Generating random numbers for
+#  games and simulations
+# Shuffling lists of data
+# Creating random passwords
+# Generating random strings
+# Performing other random operations
+# The random module is easy to use. 
+# To generate a random number, you 
+# can use the random() function. For 
+# example, the following code will 
+# generate a random number between 0 and 10:
+
+# number = random.randint(0, 10)
+# Use code with caution. Learn more
+# The random module also provides a
+#  variety of other functions for
+#  generating random numbers, such as:
+
+# randrange(): Generate a random 
+# number within a specified range
+
+# choice(): Choose a random element from a list
+
+# shuffle(): Shuffle a list in place
+
+# sample(): Return a random sample of 
+# elements from a list
+
+# The random module is a powerful
+#  tool that can be used for a
+#  variety of tasks. If you are
+#  looking for a way to generate 
+# random numbers in Python, the 
+# random module is a great option.
+
+import random
+
+
+# initialize an array to house the code to guess
+NUM_CODE = [1, 2, 3, 4, 5];
+
+
+# define constant variable which contains starting 
+# number of attempts
+ATTEMPTS = 10;
+
